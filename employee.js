81f3d854c37430a6f9e9d0a0d880f0d3d37eba0a
mrkpsa <04arun.mrks@gmail.com>
2016-09-07 18:58:16 +0530

    Add files via upload

---
+++ employee.js
@@ -0,0 +1,9 @@
+empfunc(
+	[{empid:"E6462",empname:"arun",email:"arun@gmail.com",phoneno:"8144272958",position:"Trainee software Engineer",dob:"20.05.1993",joindate:"21.3.2016"},
+{empid:"E6463",empname:"kumar",email:"kumar@gmail.com",phoneno:"9584756211",position:"software Engineer",dob:"20.05.1990",joindate:"21.3.2016"},
+{empid:"E6464",empname:"elakkiya",email:"elakkiya@gmail.com",phoneno:"8544272958",position:"Senior software Engineer",dob:"20.05.1985",joindate:"21.3.2016"},
+{empid:"E6465",empname:"mano",email:"mano@gmail.com",phoneno:"8547272958",position:"software Engineer",dob:"20.05.1990",joindate:"21.3.2016"},
+{empid:"E6466",empname:"vinoth",email:"vinoth@gmail.com",phoneno:"9857272958",position:"software Engineer",dob:"20.05.1993",joindate:"21.3.2016"},
+{empid:"E6464",empname:"priya",email:"vinoth@gmail.com",phoneno:"9857272958",position:"software Engineer",dob:"20.05.1993",joindate:"21.3.2016"}
+	]
+	);
